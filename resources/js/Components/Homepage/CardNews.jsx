e1c3941a99146ac7482ac8dad5068701c4b31452
Agiprada <78854969+Agiprada@users.noreply.github.com>
2024-01-06 22:46:56 +0800

    first commit

---
+++ resources/js/Components/Homepage/CardNews.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const isNews = (news) => {
+    return news.map((data, i) => {
+        return (
+            <div key={i} className="card w-full lg:w-96 bg-base-100 shadow-xl">
+                <figure>
+                    <img
+                        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                        alt="Shoes"
+                    />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {data.title}
+                        <div className="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>{data.description}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-inline">
+                            {data.category}
+                        </div>
+                        <div className="badge badge-outline">{data.author}</div>
+                    </div>
+                </div>
+            </div>
+        );
+    });
+};
+function noNews() {
+    return <div>Berita Tidak Tersedia</div>;
+}
+
+export default function CardNews({ news }) {
+    // if (!news && !news.length) return noNews();
+    // return isNews(news);
+    return !news ? noNews() : isNews(news);
+}
---
+++ resources/js/Pages/Homepage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/react";
+import Navbar from "@/Components/Navbar";
+import CardNews from "@/Components/Homepage/CardNews";
+import Pagination from "@/Components/Homepage/Pagination";
+
+export default function Homepage(props) {
+    return (
+        <>
+            <div className="min-h-screen bg-slate-50 ">
+                <Head title={props.title}></Head>
+                <Navbar />
+                {/* <h1>{props.description}</h1> */}
+                <div className="flex justify-center items-center flex-col lg:flex-row lg:flex-wrap lg:items-stretch gap-4 my-3">
+                    <CardNews news={props.news.data} />
+                    {/* {props.news ? (
+                        props.news.map((data, i) => {
+                            return (
+                                <div
+                                    key={i}
+                                    className="p-4 m-2 bg-white text-black shadow-md rounded-md"
+                                >
+                                    <p className="text-2xl">{data.title}</p>
+                                    <p>{data.description}</p>
+                                    <i>{data.category}</i>
+                                    <i>{data.author}</i>
+                                </div>
+                            );
+                        })
+                    ) : (
+                        <p>Not Found</p>
+                    )} */}
+                </div>
+                <div className="flex justify-center">
+                    <Pagination />
+                </div>
+            </div>
+        </>
+    );
+}
